196903be82d7abbdfbca4cdb6c85d8c75692bff5
Moxeed5 <maxmmorgan93@gmail.com>
2025-01-21 22:47:31 -0500

    erethang

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting ngrok for HTTP tunneling..."
-nohup ngrok http 3000 --config /home/max/.config/ngrok/ngrok.yml > ngrok.log 2>&1 &
+nohup ngrok http 3000 --config /home/max/.config/ngrok/ngrok.yml > ngrok.log 2>> ngrok.log &
 sleep 5  # Give ngrok time to initialize
 
 echo "Starting WiFi credential listener..."
@@ -8,6 +8,9 @@ sudo python3 wifi_listener.py
 
 echo "Registering Device...."
 go run upload_server.go
+
+echo "Fixing JSON files..."
+go run fix_json.go
 
 echo "Starting Lift Learn application..."
 sudo go run lift_learn.go
